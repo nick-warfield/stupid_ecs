61a838f341941777b387ad7d71406af54dd055b3
Nicholas Warfield <nick@nkwarfield.io>
2021-09-18 20:08:43 -0700

    Implement System getter and add some helpers to check the state of an entity

--- include/ecs.hpp
+++ include/ecs.hpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <stack>
 #include <optional>
+#include <memory>
 
-// odd generations are alive, evens are dead
 struct Entity {
 	Entity(std::size_t gen, std::size_t index)
 		: generation(gen), index(index) { }
@@ -13,16 +13,18 @@ struct Entity {
 	const std::size_t index;
 };
 
-using comp_flags = unsigned short;
+using component_flags = uint16_t;
+const component_flags COMP_ALIVE = 0b0000000000000001;
+const component_flags COMP_DATA1 = 0b0000000000000010;
+const component_flags COMP_DATA2 = 0b0000000000000100;
+
 struct ComponentConfig {
 	// just sets everything to nullopt
 	ComponentConfig();
 	// use yaml files for configs
 	ComponentConfig(std::ifstream);
 
-	// use a function so users don't need to pass in every
-	// component they want at once
-	comp_flags bitfield();
+	component_flags bitmask();
 
 	// optional<name>
 	// optional<position>
@@ -35,10 +37,22 @@ public:
 	Entity make(ComponentConfig);
 	void erase(const Entity &);
 
+	bool is_alive(const Entity&);
+	bool has_component(const Entity&, component_flags);
+
+	struct Item {
+		std::optional<std::unique_ptr<int>> data1;
+		std::optional<std::unique_ptr<std::string>> data2;
+	};
+	std::optional<Item> operator[](const Entity &);
+
 private:
 	std::stack<std::size_t> m_next_alloc;
 	std::vector<std::size_t> m_generation;
-	std::vector<comp_flags> m_component;
+	std::vector<component_flags> m_component;
+
+	std::vector<int> m_data1;
+	std::vector<std::string> m_data2;
 
 	// vec<name>
 	// vec<position>
--- src/ecs.cpp
+++ src/ecs.cpp
@@ -3,20 +3,39 @@
 Entity System::make(ComponentConfig cc) {
 	if (m_next_alloc.empty()) {
 		m_generation.push_back(0);
-		m_component.push_back(cc.bitfield());
+		m_component.push_back(cc.bitmask() & COMP_ALIVE);
 		return Entity(0, m_generation.size() - 1);
 	} else {
 		auto next = m_next_alloc.top();
 		m_next_alloc.pop();
 		m_generation[next]++;
-		m_component[next] = cc.bitfield();
+		m_component[next] = cc.bitmask() & COMP_ALIVE;
 		return Entity(m_generation[next], next);
 	}
 }
 
 void System::erase(const Entity &id) {
 	if (m_generation[id.index] != id.generation) return;
-	m_generation[id.index]++;
+	m_component[id.index] = m_component[id.index] & ~COMP_ALIVE;
 	m_next_alloc.push(id.index);
 }
 
+bool System::is_alive(const Entity& id) {
+	return (m_component[id.index] & COMP_ALIVE) == COMP_ALIVE
+		&& m_generation[id.index] == id.generation;
+}
+bool System::has_component(const Entity& id, component_flags flag) {
+	return (m_component[id.index] & flag) == flag;
+}
+
+std::optional<System::Item> System::operator[](const Entity &id) {
+	if (!is_alive(id)) return {};
+
+	Item i;
+	if (has_component(id, COMP_DATA1))
+		i.data1 = std::make_unique<int>(m_data1[id.index]);
+	if (has_component(id, COMP_DATA2))
+		i.data2 = std::make_unique<std::string>(m_data2[id.index]);
+
+	return i;
+}
