b479dc53cf536e62e4de0e33c1afcc8afd9e28be
Nicholas Warfield <nick@nkwarfield.io>
2021-09-24 20:12:00 -0700

    Implement SystemHelper to unwrap recursive structs at compile time

--- include/system.hpp
+++ include/system.hpp
@@ -8,9 +8,20 @@
 #include "component.hpp"
 #include "entity.hpp"
 
+template <typename T>
+void assign_or_push(std::vector<T>& vec, T item, size_t index) {
+	if (index >= vec.size())
+		vec.push_back(item);
+	else
+		vec[index] = item;
+}
+
 template<typename ...T>
 using Item = ComponentConfig<
 	std::reference_wrapper<T>...>;
+
+template <typename T>
+struct SystemHelper;
 
 template <typename... T>
 struct SystemData;
@@ -33,20 +44,29 @@ class System {
 
 public:
 	Entity make(ComponentConfig<T...>& cc) {
+		auto next = m_generation.size();
+		if (!m_next_alloc.empty()) {
+			next = m_next_alloc.top();
 			m_next_alloc.pop();
 			m_generation[next]++;
-			m_component[next] = COMPONENT_ALIVE | flags;
-			return Entity(m_generation[next], next);
+		} else {
+			m_generation.push_back(0);
 		}
+
+		auto flags = SystemHelper<SystemData<T...>>::make(m_data, cc, next);
+		assign_or_push<component_flag>(m_component, COMPONENT_ALIVE | flags, next);
+		return Entity(m_generation[next], next);
 	}
 
-	void erase(const Entity &id) {
+	void erase(const Entity& id) {
 		if (m_generation[id.index] != id.generation) return;
 		m_component[id.index] &= ~COMPONENT_ALIVE;
 		m_next_alloc.push(id.index);
 	}
 
 	std::optional<Item<T...>> get(const Entity &id) {
+		if (!is_alive(id)) return {};
+		return SystemHelper<SystemData<T...>>::get(m_data, m_component[id.index], id.index);
 	}
 
 	bool is_alive(const Entity &id) {
